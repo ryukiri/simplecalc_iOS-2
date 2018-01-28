73dc0ba1e282ea3e980110cb41d5230355b899e8
Austin Quach <austinquachh@gmail.com>
2018-01-28 01:18:38 -0800

    Feature: Add

--- simpleCalc-iOS/ViewController.swift
+++ simpleCalc-iOS/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
     }
     
     var total = 0
+    var totalString = ""
     var currentExpression = "+"
     var noCommand = true
     
@@ -28,109 +29,61 @@ class ViewController: UIViewController {
     @IBOutlet weak var label_answer: UILabel!
     
     @IBAction func button0(_ sender: Any) {
-        // If there is a command
-        if !noCommand {
-            if currentExpression == "+" {
-                total += 0
-            }
-            if currentExpression == "-" {
-                total -= 0
-            }
-            if currentExpression == "x" {
-                total *= 0
-                
-            }
-            if currentExpression == "/" {
-                label_answer.text = "N/A"
-                total = 0;
-            }
-            textView_work.text = textView_work.text + " 0 "
-            label_answer.text = String(total)
-        } else {
-            noCommand = true
-            textView_work.text = textView_work.text + " 0 "
-        }
+        textView_work.text = textView_work.text + " 0 "
+        totalString += "0"
     }
     
     @IBAction func button1(_ sender: Any) {
-        // If there is a command
-        if !noCommand {
-            if currentExpression == "+" {
-                total += 1
-            }
-            if currentExpression == "-" {
-                total -= 1
-            }
-            if currentExpression == "x" {
-                total *= 1
-            }
-            if currentExpression == "/" {
-                total /= 1
-            }
-            textView_work.text = textView_work.text + " 1 "
-            label_answer.text = String(total)
-        } else {
-            
-            noCommand = true
-            textView_work.text = textView_work.text + " 1 "
-        }
+        textView_work.text = textView_work.text + " 1 "
+        totalString += "1"
     }
     
     @IBAction func button2(_ sender: Any) {
-        // If there is a command
-        if !noCommand {
-            if currentExpression == "+" {
-                total += 2
-            }
-            if currentExpression == "-" {
-                total -= 2
-            }
-            if currentExpression == "x" {
-                total *= 2
-            }
-            if currentExpression == "/" {
-                total /= 2
-            }
-            textView_work.text = textView_work.text + " 2 "
-            label_answer.text = String(total)
-        } else {
-            noCommand = true
-            textView_work.text = textView_work.text + " 2 "
-        }
+        textView_work.text = textView_work.text + " 2 "
+        totalString += "2"
     }
     
     @IBAction func button3(_ sender: Any) {
         textView_work.text = textView_work.text + " 3 "
+        totalString += "3"
     }
     
     @IBAction func button4(_ sender: Any) {
         textView_work.text = textView_work.text + " 4 "
+        totalString += "4"
     }
     
     @IBAction func button5(_ sender: Any) {
         textView_work.text = textView_work.text + " 5 "
+        totalString += "5"
     }
     
     @IBAction func button6(_ sender: Any) {
         textView_work.text = textView_work.text + " 6 "
+        totalString += "6"
     }
     
     @IBAction func button7(_ sender: Any) {
         textView_work.text = textView_work.text + " 7 "
+        totalString += "7"
     }
     
     @IBAction func button8(_ sender: Any) {
         textView_work.text = textView_work.text + " 8 "
+        totalString += "8"
     }
     
     @IBAction func button9(_ sender: Any) {
         textView_work.text = textView_work.text + " 9 "
+        totalString += "9"
     }
     
     @IBAction func buttonAdd(_ sender: Any) {
         textView_work.text = textView_work.text + " + "
         currentExpression = "+"
-        noCommand = false
+        total += Int(totalString)!
+        totalString = ""
+        //label_answer.text = String(total)
     }
     
     @IBAction func buttonMinus(_ sender: Any) {
@@ -152,12 +105,19 @@ class ViewController: UIViewController {
     }
     
     @IBAction func buttonEquals(_ sender: Any) {
+        if currentExpression == "+" {
+            total += Int(totalString)!
+        }
+        
+        totalString = ""
         label_answer.text = String(total)
     }
     
     @IBAction func buttonClear(_ sender: Any) {
         textView_work.text = ""
         label_answer.text = ""
+        total = 0
+        totalString = ""
     }
     
     
